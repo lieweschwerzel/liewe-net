2682549b5ab6c8edddd57d639da61f1b6f25e3fb
lieweschwerzel <36862738+lieweschwerzel@users.noreply.github.com>
2021-04-29 18:15:34 +0200

    responsive projects index

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import { Link } from "gatsby"
 import React from "react"
 
+
 export default function Navbar() {
   return (
     <nav>
-      <Link to="/">liewe_schwerzel</Link>
+      <Link className="navlogo" to="/">liewe_schwerzel</Link>
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
--- src/projects/rent-share.md
+++ src/projects/rent-share.md
@@ -1,6 +1,6 @@
 ---
 title: RentShare
-stack: Java, Java Springboot, MySQL
+stack: Java, Spring Boot, MySQL
 slug: rent-share
 date: 2021-02-01T00:00:00+00:00
 thumb: ../images/thumbs/skate.png
