4edf1bf50755966bfbeb3e15f3c19680386824f7
lieweschwerzel <36862738+lieweschwerzel@users.noreply.github.com>
2021-05-08 15:58:27 +0200

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,23 +1,39 @@
 import React from "react"
-import Layout from "../components/Layout"
-
 import * as styles from "../styles/contact.module.css"
 
-const Contact = () => (
-    <Layout>
-      <h1>Contact</h1>
-  
-      <form name="Contact Form" method="POST" data-netlify="true">
-        <input type="hidden" name="form-name" value="Contact Form" />
-        <div>
-          <label>Your Email:</label>
-          <input type="email" name="email" />
-        </div>
-        <div>
-          <label>Message:</label>
-          <textarea name="message" />
-        </div>
-        <button type="submit">Send</button>
+export default function contact() {
+  return (
+    <div>
+      <form
+        name="contact v1"
+        method="POST"
+        data-netlify="true"
+        onSubmit="submit"
+      >
+        <input type="hidden" name="form-name" value="contact v1"/ 
+        >
+
+          <div>
+            <label>First Name <br/>
+              <input type="text" name="first-name" />
+            </label>
+          </div>
+
+          <div>
+            <label htmlFor="email">Email</label> <br/>
+            <input id="email" type="email" name="email" />
+          </div>
+
+          <div>
+            <label >Any Comments? <br/>
+              <textarea name="comments"></textarea>
+            </label>
+          </div>
+
+          <button type="submit">Submit The Results</button>
+
+
       </form>
-    </Layout>
+    </div>
   )
+}
