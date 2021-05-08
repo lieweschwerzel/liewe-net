cce2e0f912ec2a1be19fdb6866c8a825164d3cb6
lieweschwerzel <36862738+lieweschwerzel@users.noreply.github.com>
2021-05-08 16:32:37 +0200

    more contact

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/projects">Portfolio</Link>
-        <Link to="">Contact</Link>
+        <Link to="/contact">Contact</Link>
       </div>
     </nav>
   )
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,45 +1,52 @@
 import React from "react"
+import Layout from "../components/Layout"
+
 import * as styles from "../styles/contact.module.css"
 
 export default function contact() {
   return (
-    <div>
-      <form
-        name="contact v1"
-        method="POST"
-        data-netlify="true"
-        onSubmit="submit"
-       // action="/success/"
-       data-netlify-honeypot="bot-field"
-      >
-        <input type="hidden" name="form-name" value="contact v1"/ 
+    <Layout>
+      <div>
+        <form
+          name="contact v1"
+          method="POST"
+          data-netlify="true"
+          onSubmit="submit"
+          // action="/success/"
+          data-netlify-honeypot="bot-field"
         >
+          <input type="hidden" name="form-name" value="contact v1" />
 
           <div hidden>
             <input type="bot-field" />
           </div>
 
           <div>
-            <label>First Name <br/>
+            <label>
+              First Name <br />
               <input type="text" name="first-name" />
             </label>
           </div>
 
           <div>
-            <label htmlFor="email">Email</label> <br/>
+            <label htmlFor="email">
+              <br />
+              Email
+            </label>
             <input id="email" type="email" name="email" />
           </div>
 
           <div>
-            <label >Any Comments? <br/>
-              <textarea name="comments"></textarea>
+            <label>
+              <br />
+              Message <br />
+              <textarea name="comments" cols="60" rows="10"></textarea>
             </label>
           </div>
-
-          <button type="submit">Submit The Results</button>
-
-
-      </form>
-    </div>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </Layout>
   )
 }
