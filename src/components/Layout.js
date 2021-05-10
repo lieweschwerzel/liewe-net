237f795bf44b4c7ae8ea936054d545e4d8ab22be
lieweschwerzel <36862738+lieweschwerzel@users.noreply.github.com>
2021-05-10 19:53:15 +0200

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,15 @@
 import React from "react"
 import Navbar from "./Navbar"
 import "../styles/global.css"
+import { Helmet } from "react-helmet"
+import favicon from '../images/gatsby-icon.ico'
+
 
 
 export default function Layout({ children }) {
+  <Helmet>
+  <link rel="icon" href={favicon} />
+</Helmet>
     return (
     <div className="layout">      
       <Navbar />
